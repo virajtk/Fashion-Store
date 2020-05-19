6b29551b9ada0567aabe5f7c14094b7fb5cfc580
Senura Diwantha <senuradiwantha900@gmail.com>
2020-05-19 16:50:05 +0530

    Added add product  to frontend.

--- Front-End/src/components/dashboard-components/components/AddProduct.js
+++ Front-End/src/components/dashboard-components/components/AddProduct.js
@@ -3,53 +3,128 @@ import Product from "../../img/product/8.jpg";
 
 class AddProduct extends Component {
 
-    state = {
-        mainCategory: '',
-        productType: '',
-        productName: '',
-        description: '',
-        price: '',
-        brand: '',
-        colour: '',
-        status: ''
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.onChangeMainCategory = this.onChangeMainCategory.bind(this);
+
+        this.state = {
+            mainCategory: '',
+            productType: '',
+            productName: '',
+            description: '',
+            price: '',
+            discount:'',
+            brand: '',
+            colour: '',
+            productTypeList: []
+        };
+    }
+
+
+    onChangeMainCategory = (event) =>{
+        this.setState({
+            mainCategory: event.target.value
+        });
+
+        fetch('http://localhost:3000/product/findProductType/'+event.target.value)
+            .then(res => res.json())
+            .then(json =>{
+                console.log("json",json);
+                this.setState({
+                    productTypeList: json,
+                })
+            });
+
     };
 
+    onChangeSubCategory = (event) => {
+        this.setState({
+            productType: event.target.value
+        });
+    }
+
+
     handleChange = (event) => {
+        const target = event.target;
+        const  name = target.name;
+        const  value = target.value;
+
+        this.setState({
+            [name]:value
+        });
+
+    };
+
+    async postData() {
+        try {
+            let result = await fetch('http://localhost:3000/product/addproduct', {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(this.state)
+            });
+
+            console.log('Result: '+ result);
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(JSON.stringify(this.state));
+        this.postData();
 
     };
 
 
     render() {
+        const { productTypeList } = this.state;
         return (
             <div>
                 <div>
                     <h4 className="header-title">Add New Product</h4>
                 </div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="row">
                         <div className="col-md-8">
                             <div className="form-group">
                                 <label className="col-form-label">Main Category</label>
-                                <select className="custom-select style-input select-style">
+                                <select
+                                    className="custom-select style-input select-style"
+                                    name="mainCategory"
+                                    value={this.state.mainCategory}
+                                    onChange={this.onChangeMainCategory}
+                                >
                                     <option selected="selected"> </option>
                                     <option value="Woman wear">Woman wear</option>
-                                    <option value="Men wear">Men wear</option>
+                                    <option value="men-wear">Men wear</option>
                                     <option value="Children">Children</option>
                                     <option value="Bags and Purses">Bags and Purses</option>
-                                    <option value="Footwear">Footwear</option>
-                                    <option value="Jewellery">Jewellery</option>
+                                    <option value="footwear">Footwear</option>
+                                    <option value="jewellery">Jewellery</option>
                                 </select>
                             </div>
                             <div className="form-group">
                                 <label className="col-form-label">Product Type</label>
-                                <select className="custom-select style-input select-style">
+                                <select
+                                    className="custom-select style-input select-style"
+                                    name="productType"
+                                    value={this.state.productType}
+                                    onChange={this.onChangeSubCategory}
+                                >
                                     <option selected="selected"> </option>
-                                    <option value="Woman wear">Woman wear</option>
-                                    <option value="Men wear">Men wear</option>
-                                    <option value="Children">Children</option>
-                                    <option value="Bags and Purses">Bags and Purses</option>
-                                    <option value="Footwear">Footwear</option>
-                                    <option value="Jewellery">Jewellery</option>
+                                    {this.state.productTypeList.map(productTypes => (
+                                        <option key={productTypes._id} value={productTypes.subCategory}>
+                                            {productTypes.subCategory}
+                                        </option>
+                                    ))}
                                 </select>
                             </div>
                             <div className="form-group">
@@ -73,7 +148,7 @@ class AddProduct extends Component {
                                     name="description"
                                     value={this.state.description}
                                     onChange={this.handleChange}
-                                ></textarea>
+                                />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="exampleInputEmail1">Price</label>
@@ -83,6 +158,17 @@ class AddProduct extends Component {
                                     placeholder="Price"
                                     name="price"
                                     value={this.state.price}
+                                    onChange={this.handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">Discount</label>
+                                <input
+                                    type="text"
+                                    className="form-control style-input"
+                                    placeholder="Discount"
+                                    name="discount"
+                                    value={this.state.discount}
                                     onChange={this.handleChange}
                                 />
                             </div>
@@ -106,17 +192,6 @@ class AddProduct extends Component {
                                     value={this.state.colour}
                                     onChange={this.handleChange}
                                 />
-                            </div>
-                            <div className="form-check text-left">
-                                <input
-                                    type="checkbox"
-                                    className="form-check-input"
-                                    id="exampleCheck1"
-                                    name="status"
-                                    value={this.state.status}
-                                    onClick={2}
-                                />
-                                <label className="form-check-label" htmlFor="exampleCheck1">Approved</label>
                             </div>
                             <div className="fashion-buttons text-left">
                                 <button type="submit" className="btn fashion-btn ">Submit</button>
